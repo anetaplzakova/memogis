37052cfb0838d818d722448e2c9d32a49f486d0f
michalfrankl <michal.frankl@gmail.com>
2022-01-25 18:01:43 +0100

    Custom styles for MemoGIS

--- styleConfig.js
+++ styleConfig.js
@@ -2,18 +2,18 @@ const styleConfig = {
     // Base foreground (text) color
     base_fg: "#595959",
     // Top and bottom panels
-    panel_bg: "rgba(255, 255, 255, 0.75)",
-    panel_fg: "#595959",
+    panel_bg: "#595959", //hlavní panely - podklad, default rgba(255, 255, 255, 0.75)
+    panel_fg: "rgba(255, 255, 255, 0.75)", //hlavní panely - text, default #595959
     // Titlebars
-    titlebar_bg: "#595959",
-    titlebar_fg: "white",
+    titlebar_bg: "#808080",
+    titlebar_fg: "#f2f2f2",
     // Containers (i.e. dialogs, sidebars)
-    container_bg: "#f8f8f8",
+    container_bg: "#f8f8f8", //podkladová barva všech rámečků, default #f8f8f8
     // Buttonbar buttons
-    button_bg: "#e9e9e9",
+    button_bg: "#595959",//tlačítko vyhledávání +?, default #e9e9e9
     button_hover_bg: "#dcdcdc",
     // appmenu (1: main level, 2: sublevel)
-    appmenu_bg1: "#f2f2f2",
+    appmenu_bg1: "#f2f2f2",//hlavní lvl pravého menu, default #f2f2f2
     appmenu_fg1: "#595959",
     appmenu_active_bg1: "white",
     appmenu_active_fg1: "#595959",
@@ -27,10 +27,10 @@ const styleConfig = {
     map_button_active_bg: "#f2f2f2",
     map_button_active_fg: "#595959",
     // Generic
-    item_border: "#bbb",
-    item_active: "#078aa3",
-    item_active_hover: "#05768a",
-    item_inactive_hover: "#dcdcdc"
+    item_border: "#595959",//kontury, vč. vyhledávání ad., default #bbb
+    item_active: "#078aa3",//?
+    item_active_hover: "#05768a",//?
+    item_inactive_hover: "#dcdcdc"//?
 };
 
 module.exports = styleConfig;
